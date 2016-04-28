f1ec1a452a85afc5c638051db439fa77fa21ee1e
Kuznetsov Ilia <i.kuznecov@corp.mail.ru>
2016-04-28 18:09:52 +0300

    rollup issue

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-import {document} from './test';
-var d = () => document;
+var a = {prop: 0};
+var b = a.prop;
+export {
+  b, a, set
+};
+
+function set(new_a, new_b) {
+  a = new_a;
+  b = new_b;
+}
