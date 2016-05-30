b90c8fc9a680888f175252fcb4c94828e554d521
Kuznetsov Ilia <i.kuznecov@corp.mail.ru>
2016-05-30 19:06:55 +0300

    unnecessary variables while import #669

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-var a = {prop: 0};
-var b = a.prop;
-export {
-  b, a, set
-};
-
-function set(new_a, new_b) {
-  a = new_a;
-  b = new_b;
-}
+import {d} from './test';
+console.log(d);
--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-export { document };
+var d = [1,2,3];
+var h = noop || d[0];
+
+export { d }
+
+function noop(){
+  return 1;
+}
